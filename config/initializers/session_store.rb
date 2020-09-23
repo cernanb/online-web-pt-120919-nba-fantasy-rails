7e6849eb60b0c767d50e7fb400170308c50679f3
cernanb <cernanb@gmail.com>
2020-09-23 16:17:07 -0700

    Initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,5 @@
+Rails.application.config.session_store :cookie_store, :key => '_nba_fantasy'
+# if Rails.env.production == 'production'
+#   Rails.application.config.session_store :cookie_store, :key => '_nba_fantasy', domain: 
+# else
+# end
