7212bef2145dcaefab6e9adbddd68401763f9a2a
cernanb <cernanb@gmail.com>
2020-10-09 15:14:54 -0700

    Add logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :sessions, only: [:create]
   resources :users, only: [:create]
   get 'logged_in', to: 'sessions#logged_in'
+  delete 'logout', to: 'sessions#logout'
 end
