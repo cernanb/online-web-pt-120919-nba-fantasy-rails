ed731086bd17cecf5dd6746ce9b87f58e55c65e6
cernanb <cernanb@gmail.com>
2020-10-15 22:34:29 -0700

    Update session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
-if Rails.env.production == 'production'
+if Rails.env == 'production'
   Rails.application.config.session_store :cookie_store, :key => '_nba_fantasy', domain: 'nba-fantasy.netlify.app/'
 else
   Rails.application.config.session_store :cookie_store, :key => '_nba_fantasy'
