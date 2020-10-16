9d54902062f3c6f502efb9b6199a824ef001e513
cernanb <cernanb@gmail.com>
2020-10-15 23:17:38 -0700

    Update session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
-Rails.application.config.session_store :cookie_store, :key => '_nba_fantasy'
-# if Rails.env == 'production'
-#   Rails.application.config.session_store :cookie_store, :key => '_nba_fantasy', domain: 'dry-hollows-01080.herokuapp.com/'
-# else
-# end
+if Rails.env == 'production'
+  Rails.application.config.session_store :cookie_store, :key => '_nba_fantasy', domain: 'dry-hollows-01080.herokuapp.com/'
+else
+  Rails.application.config.session_store :cookie_store, :key => '_nba_fantasy'
+end
