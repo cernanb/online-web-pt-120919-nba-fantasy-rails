992cb38bb8ef605c1b198e79b270a9f5f8521508
cernanb <cernanb@gmail.com>
2020-10-16 01:19:28 -0700

    remove tld

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, :key => '_nba_fantasy', domain: 'dry-hollows-01080.herokuapp.com', tld_length: 2
+  Rails.application.config.session_store :cookie_store, :key => '_nba_fantasy', domain: 'dry-hollows-01080.herokuapp.com'
 else
   Rails.application.config.session_store :cookie_store, :key => '_nba_fantasy'
 end
