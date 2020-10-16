d9a5f265114cfa83a9b056a5d5f52a47a980e265
cernanb <cernanb@gmail.com>
2020-10-16 01:35:34 -0700

    fix

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, :key => '_nba_fantasy', domain: 'dry-hollows-01080.herokuapp.com'
+  Rails.application.config.session_store :cookie_store, :key => '_nba_fantasy', same_site: :none, secure: true
 else
   Rails.application.config.session_store :cookie_store, :key => '_nba_fantasy'
 end
